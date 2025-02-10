22bd8854cc1768f7189431569b7e7d9f2256754f
Domenik228 <akbarbrawlstarsnick@gmail.com>
2025-02-10 15:52:54 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+// var/let/const
+
+let name = prompt("Ismingizni kiriting:");
+
+
+console.log(name);
+
+
+
+let surname = prompt("Familyangizni kiriting:");
+
+
+console.log(surname);
+
+
+
+let age = prompt("Yoshingizni kiriting:");
+
+
+console.log(age);
+
+
+
+
+
+let address = prompt("Yashash manzilingizni kiriting:");
+
+
+
+console.log(address);
+
+
+let subject =prompt("Yoqtirgan faningizni kiriting:");
+
+
+console.log(subject);
